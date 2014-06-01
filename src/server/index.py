8bf8f0b8b3cea75621a0e3df5a1fb8a763825507
Simon Pratt <simon@pr4tt.com>
2014-06-01 16:50:04 -0400

    Code refactor to eliminate duplication

--- src/server/index.py
+++ src/server/index.py
@@ -37,27 +37,40 @@ def log_request():
     return True
 
 ######################################################################
+# Helper functions
+def getIntOrDie(request, name):
+    try:
+        val = int(request.params.get(name))
+    except:
+        abort(400)
+    return val
+
+def getStrOrDie(request, name):
+    val = request.params.get(name)
+    if val == None:
+        abort(400)
+    return val
+
+def logThenDie(message, exctype, value):
+    logging.info('exception type:  {}'.format(exctype))
+    logging.info('exception value: {}'.format(value))
+    logging.info(message)
+    abort(500, message)
+    
+
+######################################################################
 # GET Routes
 @app.get('/do/codeByID')
 @app.get('/do/code/<id>')
 def get_code_by_id(id = None):
-    if id == None:
-        id = request.query.get('id')
-    try:
-        id = int(id)
-    except:
-        abort(400)
+    id = id or getIntOrDie(request, 'id')
     try:
         session = Session()
         found = session.query(Code).filter(Code.id == id).first()
         if found == None:
             abort(404)
     except:
-        exctype, value = sys.exc_info()[:2]
-        logging.info('Error adding new code')
-        logging.info('exception type:  {}'.format(exctype))
-        logging.info('exception value: {}'.format(value))
-        abort(500, 'Error adding new code')
+        logThenDie('Error while getting code by id', *sys.exc_info()[:2])
     return json.dumps({
         'id' : found.id,
         'text' : found.text,
@@ -67,15 +80,8 @@ def get_code_by_id(id = None):
 @app.get('/do/commentsOnLine')
 @app.get('/do/code/<code_id>/comments/<line>')
 def get_comments(code_id = None, line = None):
-    if code_id == None:
-        code_id = request.query.get('code_id')
-    if line == None:
-        line = request.query.get('line')
-    try:
-        code_id = int(code_id)
-        line = int(line)
-    except:
-        abort(400)
+    code_id = code_id or getIntOrDie(request, 'code_id')
+    line = line or getIntOrDie(request, 'line')
     try:
         session = Session()
         comments = session.query(Comment).filter(
@@ -84,23 +90,14 @@ def get_comments(code_id = None, line = None):
         ).all()
         returnarr = [c.getDict() for c in comments]
     except:
-        exctype, value = sys.exc_info()[:2]
-        logging.info('Error getting comments on line')
-        logging.info('exception type:  {}'.format(exctype))
-        logging.info('exception value: {}'.format(value))
-        abort(500, 'Error getting comments on line')
+        logThenDie('Error getting comments on line', *sys.exc_info()[:2])
     return json.dumps(returnarr)
 
 
 @app.get('/do/commentCount')
 @app.get('/do/code/<code_id>/comments/count')
 def count_comments(code_id = None):
-    if code_id == None:
-        code_id = request.query.get('code_id')
-    try:
-        code_id = int(code_id)
-    except:
-        abort(400)
+    code_id = code_id or getIntOrDie(request, 'code_id')
     try:
         session = Session()
         counts = session.query(
@@ -110,11 +107,7 @@ def count_comments(code_id = None):
         for line, count in counts:
             returnob[line] = count
     except:
-        exctype, value = sys.exc_info()[:2]
-        logging.info('Error getting comment counts')
-        logging.info('exception type:  {}'.format(exctype))
-        logging.info('exception value: {}'.format(value))
-        abort(500, 'Error getting comment counts')
+        logThenDie('Error getting comment counts', *sys.exc_info()[:2])
     return json.dumps(returnob)
 
 @app.get('/do/anticsrf')
@@ -125,60 +118,24 @@ def get_anticsrf():
 # POST Routes
 @app.post('/do/newcode')
 def add_code():
-    text = request.forms.get('text')
-    lang = request.forms.get('lang')
-    if text == None:
-        abort(400, json.dumps({
-            'error' : 'Attribute text not provided'
-        }))
-    if lang == None:
-        abort(400, json.dumps({
-            'error' : 'Attribute lang not provided'
-        }))
+    text = getStrOrDie(request, 'text')
+    lang = getStrOrDie(request, 'lang')
     new_code = Code(text = text, lang = lang)
     try:
         session = Session()
         session.add(new_code)
         session.commit()
     except:
-        exctype, value = sys.exc_info()[:2]
-        logging.info('Error adding new code')
-        logging.info('exception type:  {}'.format(exctype))
-        logging.info('exception value: {}'.format(value))
-        abort(500, 'Error adding new code')
+        logThenDie('Error adding new code', *sys.exc_info()[:2])
     return json.dumps({'id' : new_code.id})
 
 @app.post('/do/newcomment')
 def add_comment():
-    code_id = request.forms.get('code_id')
-    text = request.forms.get('text')
-    line_start = request.forms.get('line_start')
-    line_end = request.forms.get('line_end')
-    diffs = request.forms.get('diffs')
-    if code_id == None:
-        abort(400, json.dumps({
-            'error' : 'Attribute code_id not provided'
-        }))
-    if text == None:
-        abort(400, json.dumps({
-            'error' : 'Attribute text not provided'
-        }))
-    if diffs == None:
-        abort(400, json.dumps({
-            'error' : 'Attribute diffs not provided'
-        }))
-    try:
-        line_start = int(line_start)
-    except:
-        abort(400, json.dumps({
-            'error' : 'Attribute line_start not a valid integer'
-        }))
-    try:
-        line_end = int(line_end)
-    except:
-        abort(400, json.dumps({
-            'error' : 'Attribute line_start not a valid integer'
-        }))
+    code_id = getStrOrDie(request, 'code_id')
+    text = getStrOrDie(request, 'text')
+    line_start = getIntOrDie(request, 'line_start')
+    line_end = getIntOrDie(request, 'line_end')
+    diffs = getStrOrDie(request, 'diffs')
     try:
         session = Session()
         found = session.query(Code).filter(Code.id == code_id).first()
@@ -194,11 +151,7 @@ def add_comment():
         session.add(new_comment)
         session.commit()
     except:
-        exctype, value = sys.exc_info()[:2]
-        logging.info('Error adding new comment')
-        logging.info('exception type:  {}'.format(exctype))
-        logging.info('exception value: {}'.format(value))
-        abort(500, 'Error adding new comment')
+        logThenDie('Error adding new comment', *sys.exc_info()[:2])
     return json.dumps({'id' : new_comment.id})
 
 @app.post('/do/login')
